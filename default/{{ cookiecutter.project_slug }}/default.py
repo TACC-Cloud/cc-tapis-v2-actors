06fb30ce8fe26f35e9510aeaffce078bcb235995
Shweta Gopal <sgopal@sgopal-mbp19.local>
2021-07-12 14:50:39 -0500

    correcting error

--- default/{{ cookiecutter.project_slug }}/default.py
+++ default/{{ cookiecutter.project_slug }}/default.py
@@ -3,12 +3,9 @@ from agavepy.actors import get_context
 
 
 def main():
-
-    def main():
-        context = get_context()
-        m = context['message_dict']['text']
-
-    r.logger.info("Actor received message: {}".format(m))
+    context = get_context()
+    m = context['message_dict']['text']
+    print("Actor received message: {}".format(m))
 
 if __name__ == '__main__':
     main()
